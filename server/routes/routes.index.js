7674e8607281f4a926fe3d422075d510e8766345
Ian Gurgel <iangurgel25@hotmail.com>
2025-08-17 16:30:50 -0300

    fix: auth.middleware working just fine

--- server/routes/routes.index.js
+++ server/routes/routes.index.js
@@ -5,15 +5,18 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import { handleLogin, handleRegister } from "../controllers/auth.controller.js";
 import { handleGetAllUsers, handleGetUser } from "../controllers/crud.controller.js";
+import { AuthorizeJWT } from "../auth.middleware.js";
 
 const router = Router();
 
 router.get("/api/users", handleGetAllUsers)
 
-router.get("/api/users/:_id", handleGetUser)
+router.get("/api/users/:_id", AuthorizeJWT, handleGetUser)
 
 router.post("/api/auth/register", handleRegister)
 
 router.post("/api/auth/login", handleLogin)
+
+router.post("/api/auth/logout", () => {})
 
 export default router;
